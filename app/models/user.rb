d029778c7585b01b27ab049536afaa7b9a9f66f6
z18098to <z18098to@aiit.ac.jp>
2019-04-16 18:23:36 +0000

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates name, presence: true    # 「FILL_IN」→name をコードに置き換え  #てください→削除
-  #validates email, presence: true    # 「FILL_IN」→emailをコードに置き換え #てください→削除
+  validates :name, presence: true    # 「FILL_IN」→name をコードに置き換え  #てください→削除
+  validates :email, presence: true    # 「FILL_IN」→emailをコードに置き換え #てください→削除
 end
